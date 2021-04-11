b0d2f5ba0184ed66a5511fe151eab18f97b18f4c
andrilve <andrilve@users.noreply.github.com>
2021-04-11 19:39:14 +0300

    2021.04.11

--- src/Calc/Server.java
+++ src/Calc/Server.java
@@ -6,46 +6,27 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Server {
+    static Socket socket;
 
-    //DataOutputStream out;
-
-    public Server() throws IOException {
-        System.out.println("server work");
-        /*try(Socket socket = new Socket("127.0.0.1", 32123)){
-            //    System.out.println("Connecting whit server " + socket.getInetAddress() + " port " + socket.getPort());
-            //DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            while(!socket.isOutputShutdown()){
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                System.out.println("I alive");
-                out.writeInt(5);
-                out.flush();
-                break;
-            }
-        }
-        catch (UnknownHostException e) {
+    static {
+        try {
+            socket = new Socket("127.0.0.1", 32123);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }*/
     }
 
     public void out(String value) throws IOException {
-        try(Socket socket = new Socket("127.0.0.1", 32123)){
-            //    System.out.println("Connecting whit server " + socket.getInetAddress() + " port " + socket.getPort());
-            //DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        try {
             while(!socket.isOutputShutdown()){
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 System.out.println("I alive");
                 out.writeUTF(value);
                 out.flush();
                 break;
-            }
         }
-        catch (UnknownHostException e) {
-             e.printStackTrace();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
    }
