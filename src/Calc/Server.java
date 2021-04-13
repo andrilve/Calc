e96f51c70cf76c0775f122f1afaa695a9af11a31
andrilve <andrilve@users.noreply.github.com>
2021-04-13 16:17:46 +0300

    2021.04.13 16Ж17

---
+++ src/Calc/Server.java
@@ -0,0 +1,34 @@
+package Calc;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Server {
+    static Socket socket;
+
+    static {
+        try {
+            socket = new Socket("127.0.0.1", 32123);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void out(String value) throws IOException {
+
+        try {
+            while(!socket.isOutputShutdown()){
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                System.out.println("I alive");
+                out.writeUTF(value);
+                out.flush();
+                break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
